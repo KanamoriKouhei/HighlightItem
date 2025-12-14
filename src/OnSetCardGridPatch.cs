f309c711e37aba5d8f75920930a14a10e0d564dc
Kanayan <glgln6@gmail.com>
2025-12-14 16:25:22 +0900

    feat: 武器背景色のハイライト方式を変更

--- src/OnSetCardGridPatch.cs
+++ src/OnSetCardGridPatch.cs
@@ -1,4 +1,6 @@
 using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace HighlightItem
 {
@@ -7,7 +9,7 @@ namespace HighlightItem
     {
         private static void Postfix(Trait __instance, ButtonGrid __0)
         {
-            // 商人のハイライトを有効化するためにコメントアウト
+            // 商人のイベントリ内のハイライトを有効化するためにコメントアウト
             // プレイヤーキャラクターのアイテムであるか確認
             // if (__0.invOwner?.owner?.IsPC != true)
             //     return;
@@ -21,7 +23,7 @@ namespace HighlightItem
             // 食べ物アイテムを除外
             if (card.IsFood)
                 return;
-            
+
             // エンチャントが無いアイテムを除外
             if (card.elements == null)
                 return;
@@ -31,12 +33,32 @@ namespace HighlightItem
                 // CSVファイルのエンチャント名が空白の場合を除外
                 if (string.IsNullOrEmpty(userFilter.EnchantName))
                     continue;
-                
+
                 foreach (var element in card.elements.dict.Values)
                 {
                     if (Plugin.CheckIsMatch(element, userFilter, card))
-                        // アイテム外枠をハイライト
-                        __0.Attach("searched", false);
+                    {
+                        // アイコンを表示
+                        // __0.Attach("searched", false);
+
+                        // アイテム画像の外周を黄色にする (Add Outline)
+                        // ButtonGrid自身か、そのImageコンポーネントを取得
+                        var images = __0.GetComponentsInChildren<Image>(true);
+
+                        // 1. 外周 (Main Image or Button Target Graphic)
+                        // 通常、ButtonGridの一番親にあるImageか、最初に見つかるImageが背景/枠
+                        if (images.Length > 0)
+                        {
+                            var targetImage = images[0]; // 親あるいは主要なImageと仮定
+                            var outline = targetImage.GetComponent<Outline>();
+                            if (outline == null)
+                                outline = targetImage.gameObject.AddComponent<Outline>();
+
+                            outline.effectColor = Color.white;
+                            outline.effectDistance = new Vector2(1, -1);
+                            outline.enabled = true;
+                        }
+                    }
                 }
             }
         }
