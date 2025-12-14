92eb1402822e46ac25b4f7f8fe2f2f79ed1ce4ca
Kanayan <glgln6@gmail.com>
2025-12-14 18:47:36 +0900

    revert: 背景表示修正を無くしてアイコン表示形式に戻す

--- src/OnSetCardGridPatch.cs
+++ src/OnSetCardGridPatch.cs
@@ -1,6 +1,4 @@
 using HarmonyLib;
-using UnityEngine;
-using UnityEngine.UI;
 
 namespace HighlightItem
 {
@@ -9,7 +7,7 @@ namespace HighlightItem
     {
         private static void Postfix(Trait __instance, ButtonGrid __0)
         {
-            // 商人のイベントリ内のハイライトを有効化するためにコメントアウト
+            // 商人のハイライトを有効化するためにコメントアウト
             // プレイヤーキャラクターのアイテムであるか確認
             // if (__0.invOwner?.owner?.IsPC != true)
             //     return;
@@ -37,28 +35,8 @@ namespace HighlightItem
                 foreach (var element in card.elements.dict.Values)
                 {
                     if (Plugin.CheckIsMatch(element, userFilter, card))
-                    {
-                        // アイコンを表示
-                        // __0.Attach("searched", false);
-
-                        // アイテム画像の外周を黄色にする (Add Outline)
-                        // ButtonGrid自身か、そのImageコンポーネントを取得
-                        var images = __0.GetComponentsInChildren<Image>(true);
-
-                        // 1. 外周 (Main Image or Button Target Graphic)
-                        // 通常、ButtonGridの一番親にあるImageか、最初に見つかるImageが背景/枠
-                        if (images.Length > 0)
-                        {
-                            var targetImage = images[0]; // 親あるいは主要なImageと仮定
-                            var outline = targetImage.GetComponent<Outline>();
-                            if (outline == null)
-                                outline = targetImage.gameObject.AddComponent<Outline>();
-
-                            outline.effectColor = Color.white;
-                            outline.effectDistance = new Vector2(1, -1);
-                            outline.enabled = true;
-                        }
-                    }
+                        // アイテム外枠をハイライト
+                        __0.Attach("searched", false);
                 }
             }
         }
